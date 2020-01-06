0bbfadeec429eb582059e634747de66ff021ed99
ch <2564510610@qq.com>
2020-01-06 19:01:08 +0800

    some_style

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -5,7 +5,7 @@ import './style.less';
 
 export default class extends PureComponent {
     render() {
-        const { props: { title = "", path = "#", color = "true" } } = this
+        const { props: { title = "", path = "#", color } } = this
         return (
             <Link className={cs({ component_nav: true ,component_nav_black: color })} to={path}>
                 {title}
