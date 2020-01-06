cc6e9126838e9127d6ce2217490743e6428a6e9c
ch <2564510610@qq.com>
2020-01-06 18:49:01 +0800

    格式严禁

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import './styles.less'
 
-
 export default class extends React.PureComponent {
-    componentDidMount() {
-    }
-   
-
     render() {
         return (
             <div className='pages_login'>
-                
                 pages_login
-
-
-
             </div>
         )
     }
