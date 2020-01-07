ed3c9f97824219d7616b997956e4cfe058c5feb6
wht <3054955228@qq.com>
2020-01-07 21:08:59 +0800

    '解决冲突'

--- src/actions/samplesList.js
+++ src/actions/samplesList.js
@@ -1,20 +1,9 @@
 import { createActions } from 'redux-actions'
 import { table } from '@/services'
-<<<<<<< HEAD
 import { SAMPLES_LIST, POST_TABDATA} from '@/constants/actionTypes'
 import { list } from '@/services'
-
-export const samples_list = createActions({
-    [POST_TABDATA]: options => table(options),
-    [SAMPLES_LIST]: options => list(options)
-})
-=======
-import { SAMPLES_LIST, } from '@/constants/actionTypes'
-import { list } from '@/services'
-import { POST_TABDATA } from '@/constants/actionTypes'
 
 export const samples_list = createActions({
     [SAMPLES_LIST]: options => list(options),
     [POST_TABDATA]: options => table(options)
 })
->>>>>>> wang
--- src/reducer/samplesList.js
+++ src/reducer/samplesList.js
@@ -2,7 +2,6 @@ import { handleActions } from 'redux-actions'
 import { SAMPLES_LIST, } from '@/constants/actionTypes'
 import { POST_TABDATA } from '@/constants/actionTypes'
 
-import { POST_TABDATA } from '@/constants/actionTypes'
 const initState = {
     tabData: [],
     result: [],
@@ -10,21 +9,13 @@ const initState = {
 }
 
 export default handleActions({
-<<<<<<< HEAD
     [POST_TABDATA]: (state, action) => ({ ...state, tabData: action.payload.data.result.list }),
-=======
->>>>>>> wang
     [SAMPLES_LIST]: (state, action) => {
         return {
             ...state, 
             result: action.payload.data.result.list,
             count: action.payload.data.result.count
         }
-<<<<<<< HEAD
     }
-=======
-    },
-    [POST_TABDATA]: (state, action) => ({ ...state, tabData: action.payload.data.result.list }),
 
->>>>>>> wang
 }, initState)
