f9713a463a96982738066108300faa1373730246
lxc <3121870138@qq.com>
2020-01-07 18:34:51 +0800

    'nav组件修改'

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,13 +1,17 @@
 import React, { PureComponent } from 'react'
 import { Link } from 'react-router-dom';
 import cs from 'classnames';
+import { withRouter } from 'react-router-dom'
 import './style.less';
 
-export default class extends PureComponent {
+export default @withRouter
+class extends PureComponent {
     render() {
         const { title, path, color } = this.props
+        console.log(this.props.location.pathname, 'props');
+        
         return (
-            <Link className={cs({ component_nav: true ,component_nav_black: color })} to={path}>
+            <Link className={cs({ component_nav: true ,component_nav_black: this.props.location.pathname === path })} to={path}>
                 {title}
             </Link>
         )
