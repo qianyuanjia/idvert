cd3a9edb6dcde1f5983a58dff31ed10e0b0a88b7
zhm <2513327391@qq.com>
2020-01-07 20:49:49 +0800

    提交

--- src/actions/samplesList.js
+++ src/actions/samplesList.js
@@ -1,15 +1,9 @@
 import { createActions } from 'redux-actions'
 import { table } from '@/services'
-<<<<<<< HEAD
-import { SAMPLES_LIST, } from '@/constants/actionTypes'
+import { SAMPLES_LIST, POST_TABDATA} from '@/constants/actionTypes'
 import { list } from '@/services'
 
 export const samples_list = createActions({
-    POST_TABDATA: options => table(options),
+    [POST_TABDATA]: options => table(options),
     [SAMPLES_LIST]: options => list(options)
-=======
-import { POST_TABDATA } from '@/constants/actionTypes'
-export const samples_list = createActions({
-    [POST_TABDATA]: options => table(options)
->>>>>>> wang
 })
