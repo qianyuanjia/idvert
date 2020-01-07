e1961b59025e549f1ca50b9e673982df963e38ac
ch <2564510610@qq.com>
2020-01-07 20:50:49 +0800

    list

--- src/reducer/samplesList.js
+++ src/reducer/samplesList.js
@@ -1,10 +1,5 @@
 import { handleActions } from 'redux-actions'
-<<<<<<< HEAD
-import { SAMPLES_LIST, } from '@/constants/actionTypes'
-
-=======
 import { POST_TABDATA } from '@/constants/actionTypes'
->>>>>>> wang
 const initState = {
     tabData: [],
     result: [],
@@ -12,17 +7,6 @@ const initState = {
 }
 
 export default handleActions({
-<<<<<<< HEAD
-    POST_TABDATA: (state, action) => ({ ...state, tabData: action.payload }),
-    [SAMPLES_LIST]: (state, action) => {
-        return {
-            ...state, 
-            result: action.payload.data.result.list,
-            count: action.payload.data.result.count
-        }
-    }
-=======
     [POST_TABDATA]: (state, action) => ({ ...state, tabData: action.payload.data.result.list }),
 
->>>>>>> wang
 }, initState)
