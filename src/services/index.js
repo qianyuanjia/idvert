21616437162ffc9f1073f9e133fea4769525aca0
ch <2564510610@qq.com>
2020-01-07 16:31:03 +0800

    put_502_待解决

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,5 @@
+import { requestPost } from '@/utils/request'
+import api from './api'
 // 张慧敏 api
 
 // 李晓超 api
@@ -5,3 +7,4 @@
 // 王红涛 api
 
 // 崔亚俊 api
+export const homt_add_api = options => requestPost(api.Put, options)
