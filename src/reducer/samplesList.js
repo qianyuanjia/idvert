c22f7fb48854bbb14ad124da2498429dc780cffc
wht <3054955228@qq.com>
2020-01-07 21:02:21 +0800

    '解决冲突'

--- src/reducer/samplesList.js
+++ src/reducer/samplesList.js
@@ -1,10 +1,7 @@
 import { handleActions } from 'redux-actions'
-<<<<<<< HEAD
 import { SAMPLES_LIST, } from '@/constants/actionTypes'
 
-=======
 import { POST_TABDATA } from '@/constants/actionTypes'
->>>>>>> wang
 const initState = {
     tabData: [],
     result: [],
@@ -12,17 +9,13 @@ const initState = {
 }
 
 export default handleActions({
-<<<<<<< HEAD
-    POST_TABDATA: (state, action) => ({ ...state, tabData: action.payload }),
     [SAMPLES_LIST]: (state, action) => {
         return {
             ...state, 
             result: action.payload.data.result.list,
             count: action.payload.data.result.count
         }
-    }
-=======
+    },
     [POST_TABDATA]: (state, action) => ({ ...state, tabData: action.payload.data.result.list }),
 
->>>>>>> wang
 }, initState)
