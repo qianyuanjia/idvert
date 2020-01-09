b066472c112b5dd494de672679bc233e366cf7b0
wht <3054955228@qq.com>
2020-01-09 18:45:57 +0800

    '时间'

--- src/pages/form_time/index.jsx
+++ src/pages/form_time/index.jsx
@@ -2,24 +2,53 @@ import React, { Component } from 'react';
 import { DatePicker } from 'antd';
 
 export default class index extends Component {
-
-    onChange = (date, dateString) => {
-    console.log(date, dateString);
+    state = {
+        startValue: '',
+        endValue: '',
     }
 
+    onStartChange = (date, dateString)  => {
+        this.setState({startValue: date})
+    }
+
+    onChangeEnd = (date, dateString) => {
+        this.setState({endValue: date})
+    }
+
+    disabledStartDate = startValue => {
+        const { endValue } = this.state;
+        if (!startValue || !endValue) {
+          return false;
+        }
+        return startValue.valueOf() >= endValue.valueOf();
+    }
+    
+
+    disabledEndDate = endValue => {
+        const { startValue } = this.state;
+        if (!endValue || !startValue) {
+          return false;
+        }
+        return endValue.valueOf() <= startValue.valueOf();
+    };
+    
+
     render() {
+        //const { startValue, endValue } = this.state
         return (
             <>
                 <div>
-                    <DatePicker 
-                        onChange={this.onChange} 
+                    <DatePicker  
+                        onChange={this.onStartChange} 
                         placeholder="First Seen"
+                        disabledDate={this.disabledStartDate}
                     />
                 </div>
                 <div>
                 <DatePicker 
-                        onChange={this.onChange} 
+                        onChange={this.onEndChange} 
                         placeholder="Last Seen"
+                        disabledDate={this.disabledEndDate}
                     />
                 </div>
             </>
